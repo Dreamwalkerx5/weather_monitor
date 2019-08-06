ca993efb779b90599bfc18b2b5125e2b2eb527e5
Steven Taylor <43670847+Dreamwalkerx5@users.noreply.github.com>
2019-08-06 22:10:31 +0100

    Adjust sunrise/sunset times for BST

--- weather_parser.py
+++ weather_parser.py
@@ -1,4 +1,8 @@
 #  Copyright (c) 2019. Steven Taylor. All rights reserved.
+import json
+import time
+from datetime import datetime
+
 import requests
 
 
@@ -20,17 +24,28 @@ class WeatherParser:
         data = requests.get(self.api_call_current + self.city).json()
 
         if data['cod'] != '404':
-            forecast = data['main']
-            print(forecast)
+            forecast = CurrentWeather(data['weather'][0]['description'], data['main']['temp'],
+                                      data['main']['temp_min'], data['main']['temp_max'],
+                                      data['main']['humidity'], data['visibility'],
+                                      data['main']['pressure'], data['wind']['speed'],
+                                      data['wind']['deg'], data['clouds']['all'],
+                                      data['sys']['sunrise'], data['sys']['sunset'])
+            print(forecast.sunset)
             return forecast
 
         else:
             return forecast
 
+    @staticmethod
+    def save_json(json_data):
+        # Save data to disk as json
+        with open('data_file.json', 'w') as write_file:
+            json.dump(json_data, write_file, indent=4)
+
 
 class CurrentWeather:
     def __init__(self, description='', temp=0, min_temp=0, max_temp=0, humidity=0, visibility=0,
-                 pressure=0, wind_speed=0, wind_direction=0, clouds=0, sunrise=0, sunset=0):
+                 pressure=0, wind_speed=0, wind_direction=0, clouds=0, sunrise=0, sunset=0, datatime=None):
         self.description = description
         self.temp = str(int(temp - 273.15))
         self.min_temp = str(int(min_temp - 273.15))
@@ -41,8 +56,16 @@ class CurrentWeather:
         self.wind_speed = wind_speed
         self.wind_direction = CurrentWeather.degrees_to_cardinal(wind_direction)
         self.clouds = clouds
-        self.sunrise = sunrise
-        self.sunset = sunset
+
+        # Add one hour if British summer time
+        if time.localtime().tm_isdst == 1:
+            sunrise += 3600
+            sunset += 3600
+
+        self.sunrise = datetime.utcfromtimestamp(sunrise)
+        self.sunrise = self.sunrise.strftime('%H:%M')
+        self.sunset = datetime.utcfromtimestamp(sunset)
+        self.sunset = self.sunset.strftime('%H:%M')
 
     @staticmethod
     def degrees_to_cardinal(d):
